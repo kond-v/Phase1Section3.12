03dcce7182a87a7b20db1992d1cf423647effb3e
Venkata K <venkata.kondepudi@dell.com>
2022-03-27 15:53:49 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System;
+using System.Text;
+
+doApp();
+
+static void doApp()
+{
+    string dir = Environment.CurrentDirectory;
+    string filename = Path.Combine(dir, "data.txt");
+    if (File.Exists(filename))
+    {
+        Console.WriteLine("File {0} exists.", filename);
+        string [] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+    else { 
+        Console.WriteLine("No file {0} exists", filename); 
+        using (StreamWriter swr = File.CreateText(filename))
+        {
+            swr.WriteLine("This is line one");
+            swr.WriteLine("This is line two");
+            swr.WriteLine("This is line three");
+            swr.WriteLine("This is line four");
+            swr.WriteLine("This is line five");
+        }
+        Console.WriteLine(filename + "created");
+
+    }
+    return;
+}
